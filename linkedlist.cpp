c50c16b73f6aefe168933622b07efaf5fc40b036
utsavpaudel <utsavpaudel111@gmail.com>
2019-12-05 11:01:21 +0545

    edited

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -12,6 +12,10 @@ void LinkedList::addToHead(int data){
 	newNode->info= data;
 	newNode->next = HEAD;
 	HEAD= newNode;
+	if (TAIL==NULL)
+	{
+	TAIL= HEAD;
+	}
 }
 
 void LinkedList:: traverse(){
@@ -24,10 +28,37 @@ void LinkedList:: traverse(){
 	cout<< endl;
 }
 
+void LinkedList:: addToTail(int data){
+	Node *newNode = new Node();
+	newNode->info = data;
+	newNode->next = NULL;
+	TAIL-> next = newNode;
+	TAIL = newNode;
+}
+
+void LinkedList :: removeFromHead(){
+	Node* nodeToDelet = new Node();
+	nodeToDelet = HEAD;	
+	HEAD = nodeToDelet->next;
+	delete nodeToDelet;
+
+}
+
+void Stack:: 
+	
+
 int main(){
 	LinkedList l;
-	l.addToHead(5);l.addToHead(7);l.addToHead(6);l.addToHead(8);
-	l.traverse();
-
+	l.addToHead(5);l.addToHead(7);l.addToHead(6); l.addToTail(10);//l.removeFromHead();
+	//l.traverse();
+	
+	
+	
+	/*Node *p = new Node();
+	bool found = l.retrieve(7,p);
+	
+	if (found){
+	l.add(12,p)
+	}*/
 }
  
--- linkedlist.h
+++ linkedlist.h
@@ -16,6 +16,18 @@ class LinkedList {
 	bool isEmpty();
 	void addToHead(int data);
 	void traverse();
+	void addToTail(int data);
+	void removeFromHead();
+
+};
+
+class Stack{
+	private:
+	LinkedList l;
+	public:
+	void push(int data);
+	int pop();
+
 };
 
 
